88e34b14f57e163c30db8a36326799e61c8d7744
GoliaTech <damian.pwozniak@gmail.com>
2024-05-25 21:24:49 +0200

    start.ts
    Worked on start.ts

--- src/start.ts
+++ src/start.ts
@@ -9,26 +9,17 @@ require("dotenv").config();
  */
 async function startBot() {
 	try {
-		// this is where we will start the bot.
-		// It will use sharding.
 
-		// 1. We will need to check the bot token.
-		// Since this will be both for production and development, we will need to check
-		// what argument we are running, then check if we have that token.
-		// That will have to be done in a separate function.
-		// 2. Start the bot with the shards...
+		// Skeleton
+		const manager: ShardingManager = new ShardingManager(path.join(__dirname, "bot.js"), { token: process.env.TOKEN });
+
+		manager.on("shardCreate", (shard) => {
+			shards.set(shard.id, shard);
+			console.log(`Launched shard ${shard.id}`);
+		});
+
+		await manager.spawn();
 	} catch (e: any) { throw new Error(e.message); }
 }
 
-// This basically runs when you run the command.
-// It could probably be done in a better way, but this is what we have.
-(async () => {
-	try { await startBot(); } catch (e) {
-		console.error("Logging error in START.TS");
-		console.error(`[ERROR] ${new Date().toUTCString()}\nAn unexpected error occurred!`);
-		console.error(e);
-		process.exit(1);
-	}
-})();
-
-// We will have to figure out a way to kill the shards when killing the bot.
+startBot();
