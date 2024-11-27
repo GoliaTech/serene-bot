6b16056ad0b035443aac097f0cd20ae1694d63a7
GoliaTech <damian.pwozniak@gmail.com>
2024-11-27 16:40:07 +0100

    a much better dice roll

--- src/bot/commands/rp/rpdiceroll.ts
+++ src/bot/commands/rp/rpdiceroll.ts
@@ -1,25 +1,24 @@
 import { randomInt } from "crypto";
 import { I_Command } from "../../../utilities/interface";
 import { commandBuilder } from "../../misc/builders";
-import { logInfo } from "../../../utilities/utilities";
 
-const rpCreate: I_Command = {
+const rpdiceroll: I_Command = {
 	data: commandBuilder(
 		"rpdiceroll",
-		"Roll thy dice. By default it will just list the die. Use optional argument to adjust results.",
+		"Roll thy dice. Customize the number of dice, sides, and display.",
 		{
 			dm: false
 		}
 	)
 		.addIntegerOption((amount) => amount
 			.setName("amount")
-			.setDescription("How many die you want to roll?")
+			.setDescription("How many dice do you want to roll?")
 			.setMaxValue(20)
 			.setMinValue(1)
 			.setRequired(true))
 		.addIntegerOption((sides) => sides
 			.setName("sides")
-			.setDescription("Amount of sides the die has.")
+			.setDescription("How many sides does each die have?")
 			.setChoices(
 				{ name: "2", value: 2 },
 				{ name: "3", value: 3 },
@@ -29,19 +28,22 @@ const rpCreate: I_Command = {
 				{ name: "10", value: 10 },
 				{ name: "12", value: 12 },
 				{ name: "20", value: 20 },
+				{ name: "40", value: 40 },
 				{ name: "50", value: 50 },
+				{ name: "60", value: 60 },
 				{ name: "100", value: 100 },
 			)
 			.setRequired(true))
 		.addStringOption((method) => method
 			.setName("display")
-			.setDescription("If you'd like to display different result, adjust it here.")
+			.setDescription("Customise how results are displayed.")
 			.setChoices(
-				{ name: "List", value: "list" },
+				{ name: "List (default)", value: "list" },
 				{ name: "Sum", value: "sum" },
 				{ name: "Highest", value: "max" },
 				{ name: "Lowest", value: "min" },
 				{ name: "Average", value: "avg" },
+				{ name: "Median", value: "med" },
 				{ name: "Ascending", value: "sortasc" },
 				{ name: "Descending", value: "sortdesc" },
 				{ name: "All", value: "all" }
@@ -51,88 +53,66 @@ const rpCreate: I_Command = {
 	 * @param interaction The interaction that triggered this command.
 	 */
 	async execute(interaction) {
-		// choosing the race.
-		let amount = interaction.options.getInteger("amount");
-		let sides = interaction.options.getInteger("sides");
+		const amount = interaction.options.getInteger("amount")!;
+		const sides = interaction.options.getInteger("sides")!;
+		const display = interaction.options.getString("display")!;
 
 		if (!amount || !sides) {
-			await interaction.reply({ content: "You have to choose a thing.", ephemeral: true });
+			await interaction.reply({ content: "You have to choose a number of dice and sides.", ephemeral: true });
 			return;
 		}
 
-		const table = [];
-		const tableToSortDesc = [];
-		const tableToSortAsc = [];
-		for (let i = 0; i < amount; i++) {
-			const result = randomInt(1, sides);
-			table.push(result);
-			tableToSortDesc.push(result);
-			tableToSortAsc.push(result);
-		}
-
-		const sum = table.reduce((a, b) => a + b, 0);
-
-		const max = Math.max(...table);
-		const min = Math.min(...table);
-
-		let average = sum / amount;
-		if (average % 1 < 0.5) {
-			average = Math.floor(average);
+		// Generate random rolls.
+		// this creates array from the specified amount. The callback function uses randomint from crypto package. +1 is for inclusive max.
+		const rolls = Array.from({ length: amount }, () => randomInt(sides) + 1);
+		const sum = rolls.reduce((total, roll) => total + roll, 0);
+		const max = Math.max(...rolls);
+		const min = Math.min(...rolls);
+		let avg = sum / amount;
+		if (avg % 1 < 0.5) {
+			avg = Math.floor(avg);
 		} else {
-			average = Math.ceil(average);
+			avg = Math.ceil(avg);
 		}
 
-		// I dont get it. When I did this, ALL the tables got formatted, why????
-		// const tableToSortDesc = table;
-		// const tableToSortAsc = table;
+		// Median
+		const sortedRolls = [...rolls].sort((a, b) => a - b);
+		const median = amount % 2 === 0 ? (sortedRolls[amount / 2 - 1] + sortedRolls[amount / 2]) / 2 : sortedRolls[Math.floor(amount / 2)];
 
-		console.log(tableToSortAsc);
-		console.log(tableToSortDesc);
-		console.log(table);
+		// Display
+		const displays: { [key: string]: string; } = {
+			list: rolls.join(", "),
+			sum: `${sum}`,
+			max: `${max}`,
+			min: `${min}`,
+			avg: `${avg}`,
+			med: `${median}`,
+			sortasc: sortedRolls.join(", "),
+			sortdesc: [...sortedRolls].reverse().join(", "),
+			all: `
+        	Rolls: ${rolls.join(", ")}
+        	Ascending: ${sortedRolls.join(", ")}
+        	Descending: ${[...sortedRolls].reverse().join(", ")}
+        	Sum: ${sum}
+        	Highest: ${max}
+        	Lowest: ${min}
+        	Average: ${avg}
+        	Median: ${median}
+      		`,
+		};
 
-		const sortdesc = tableToSortDesc.sort((a, b) => b - a);
-		const sortasc = tableToSortAsc.sort((a, b) => a - b);
+		const result = displays[display || "list"] || rolls.join(", ");
 
-		console.log("after sort");
-		console.log(sortdesc);
-		console.log(sortasc);
-		console.log(tableToSortAsc);
-		console.log(tableToSortDesc);
-		console.log(table);
-
-		let display = interaction.options.getString("display");
-		let result = "";
-		if (!display || display == "list") {
-			result = table.join(", ");
-		} else if (display == "sum") {
-			result = String(sum);
-		} else if (display == "max") {
-			result = String(max);
-		} else if (display == "min") {
-			result = String(min);
-		} else if (display == "avg") {
-			result = String(average);
-		} else if (display == "sortasc") {
-			result = sortasc.join(", ");
-		} else if (display == "sortdesc") {
-			result = sortdesc.join(", ");
-		} else if (display == "all") {
-			result = `${table.join(", ")}\nAscending: ${sortasc.join(", ")}\nDescending: ${sortdesc.join(", ")}\nSum: ${sum}\nHighest: ${max}\nLowest: ${min}\nAverage: ${average}`;
-		} else {
-			result = table.join(", ");
-		}
-
-		// end of command
 		await interaction.reply({
-			content: result, ephemeral: false
+			content: result
 		});
 		return;
 	}
 };
 
 module.exports = [
-	rpCreate
+	rpdiceroll
 ];
 
 // At the moment this is not very interactive, because I just wanted something done.
-// I can later upgrade it to a more interactive thing.
+// I can later upgrade it to a more interactive thing.;
