feb81e66fef05824378a1b007ac0d56e82c1f80d
GoliaTech <damian.pwozniak@gmail.com>
2024-11-30 15:57:17 +0100

    asd

---
+++ src/bot/commands/rp/rpcreatetwo.ts
@@ -0,0 +1,71 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, StringSelectMenuBuilder } from "discord.js";
+import { I_Command } from "../../../utilities/interface";
+import { commandBuilder } from "../../misc/builders";
+
+const rpCreate: I_Command = {
+	data: commandBuilder(
+		"rpcreatetwo",
+		"Create your character. You can read more about races and naming conventions on the wiki.",
+		{
+			dm: true
+		}
+	),
+	options: {
+		botOwner: true,
+	},
+	/**
+	 * The execute function for the rpcreate command.
+	 * @param interaction The interaction that triggered this command.
+	 */
+	async execute(interaction: any) {
+		const genderSelect = new StringSelectMenuBuilder()
+			.setCustomId("rp_gender_select")
+			.setPlaceholder("Select your gender")
+			.setOptions([
+				{ label: "Male", value: "male" },
+				{ label: "Female", value: "female" },
+				{ label: "Non-binary", value: "non_binary" },
+			]);
+
+		const raceSelect = new StringSelectMenuBuilder()
+			.setCustomId("rp_race_select")
+			.setPlaceholder("Select your race")
+			.setOptions([
+				{ label: "Human", value: "human" },
+				{ label: "Dracon - biped", value: "draconbiped" },
+				{ label: "Slithean - biped", value: "slitheanbiped" },
+				{ label: "Wolf", value: "wolf" },
+				{ label: "Fox", value: "fox" },
+				{ label: "Deer", value: "deer" },
+				{ label: "Equine - alicorn", value: "alicorn" },
+				{ label: "Equine - unicorn", value: "unicorn" },
+				{ label: "Equine - pegasus", value: "pegasus" },
+				{ label: "Equine", value: "equine" },
+				{ label: "Fyriian", value: "fyriian" },
+			]);
+
+		const buttonOK = new ButtonBuilder()
+			.setCustomId("rp_create_ok")
+			.setLabel("OK")
+			.setStyle(ButtonStyle.Success);
+
+		const buttonCancel = new ButtonBuilder()
+			.setCustomId("rp_create_cancel")
+			.setLabel("Cancel")
+			.setStyle(ButtonStyle.Danger);
+
+		const genderRow = new ActionRowBuilder().addComponents(genderSelect);
+		const raceRow = new ActionRowBuilder().addComponents(raceSelect);
+		const buttonRow = new ActionRowBuilder().addComponents(buttonOK, buttonCancel);
+
+		await interaction.reply({
+			components: [genderRow, raceRow, buttonRow],
+			content: "Select your gender and race.",
+			ephemeral: true
+		});
+	}
+};
+
+module.exports = [
+	rpCreate
+];
--- src/bot/events/interaction.ts
+++ src/bot/events/interaction.ts
@@ -7,6 +7,68 @@ import { findOrCreateDiscordServer } from "../../database/dao/guild";
 import { loadCommands } from "../misc/loaders";
 const embed = embedBuilder("Error", EmbedColors.error);
 const commands = loadCommands();
+
+async function commandInteraction(interaction: ChatInputCommandInteraction) {
+	// Now check if the command provided is correct.
+	if (!commands) {
+		console.log("commands not loaded???");
+		return;
+	}
+	const command = commands.get(interaction.commandName);
+	console.log(commands);
+	console.log(command);
+	if (!command) {
+		// If the command doesn't exist, don't do anything.
+		embed.setDescription("Command was not found.");
+		interaction.reply({ embeds: [embed], ephemeral: true });
+		return;
+	}
+
+	// I want to know in development what we are getting, this is not necessary, but its fun to know.
+	if (process.env.NODE_ENV === "development") {
+		console.info(command);
+	}
+
+	// Check if we are the bot owner.
+	if (command.options?.botOwner && interaction.user.id !== process.env.OWNER_ID) {
+		embed.setDescription("You are not allowed to use this command, only Bot Owner can.");
+		interaction.reply({ embeds: [embed], ephemeral: true });
+		return;
+	}
+
+	// we should check if this is dm or not, then check what role we have the right perms.
+
+	if (!interaction.channel?.isDMBased()) {
+		const guild = await interaction.client.guilds.fetch(String(interaction.guildId));
+		const serverSettings = await findOrCreateDiscordServer(guild.id);
+		const member = await guild.members.fetch(interaction.user.id);
+		if (serverSettings.error) {
+			embed.setDescription("We are sorry, it seems we have encountered an error whilst trying to get server settings.");
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			return;
+		}
+		// This never happens as .data can only be string if error is true. But TS screams otherwise so.
+		if (typeof (serverSettings.data) == "string") {
+			return;
+		}
+		if (command.options?.owner && interaction.user.id !== serverSettings.data.ownerId && interaction.user.id !== process.env.OWNER_ID) {
+			embed.setDescription("You are not the owner of the server.");
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			return;
+		}
+		if (command.options?.staff && !member.roles.resolveId(serverSettings.data.staffRole) && interaction.user.id !== process.env.OWNER_ID) {
+			embed.setDescription("You do not have the required role to use the command.");
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			return;
+		}
+	}
+
+	command.execute(interaction);
+	return;
+}
+
+
+
 /**
  * This is the main interaction function.
  */
@@ -17,70 +79,27 @@ const interaction: I_BotEvent = {
 	 * @param interaction The interaction.
 	 * @returns It returns nothing
 	 */
-	async execute(interaction: ChatInputCommandInteraction) {
+	async execute(interaction: any) {
 		try {
 			// If the command is not an interaction, return.
-			if (!interaction.isCommand()) {
-				// For development purposes, log it.
-				if (process.env.NODE_ENV === "development") console.log(`[${new Date().toUTCString()}] - Command was not an interaction`);
+			// if (!interaction.isCommand()) {
+			// 	// For development purposes, log it.
+			// 	if (process.env.NODE_ENV === "development") console.log(`[${new Date().toUTCString()}] - Command was not an interaction`);
+			// 	return;
+			// }
+			if (interaction.isChatInputCommand()) {
+				await commandInteraction(interaction);
 				return;
-			}
-
-			// Now check if the command provided is correct.
-			if (!commands) {
-				console.log("commands not loaded???");
+			} else if (interaction.isButton()) {
+				console.log("Button interaction");
 				return;
-			}
-			const command = commands.get(interaction.commandName);
-			console.log(commands);
-			console.log(command);
-			if (!command) {
-				// If the command doesn't exist, don't do anything.
-				embed.setDescription("Command was not found.");
-				interaction.reply({ embeds: [embed], ephemeral: true });
+			} else if (interaction.isStringSelectMenu()) {
+				console.log("StringSelectMenu interaction");
 				return;
+			} else {
+				console.log("Interaction was not a predefined interaction type;");
 			}
 
-			// I want to know in development what we are getting, this is not necessary, but its fun to know.
-			if (process.env.NODE_ENV === "development") {
-				console.info(command);
-			}
-
-			// Check if we are the bot owner.
-			if (command.options?.botOwner && interaction.user.id !== process.env.OWNER_ID) {
-				embed.setDescription("You are not allowed to use this command, only Bot Owner can.");
-				interaction.reply({ embeds: [embed], ephemeral: true });
-				return;
-			}
-
-			// we should check if this is dm or not, then check what role we have the right perms.
-
-			if (!interaction.channel?.isDMBased()) {
-				const guild = await interaction.client.guilds.fetch(String(interaction.guildId));
-				const serverSettings = await findOrCreateDiscordServer(guild.id);
-				const member = await guild.members.fetch(interaction.user.id);
-				if (serverSettings.error) {
-					embed.setDescription("We are sorry, it seems we have encountered an error whilst trying to get server settings.");
-					interaction.reply({ embeds: [embed], ephemeral: true });
-					return;
-				}
-				// This never happens as .data can only be string if error is true. But TS screams otherwise so.
-				if (typeof (serverSettings.data) == "string") {
-					return;
-				}
-				if (command.options?.owner && interaction.user.id !== serverSettings.data.ownerId && interaction.user.id !== process.env.OWNER_ID) {
-					embed.setDescription("You are not the owner of the server.");
-					interaction.reply({ embeds: [embed], ephemeral: true });
-					return;
-				}
-				if (command.options?.staff && !member.roles.resolveId(serverSettings.data.staffRole) && interaction.user.id !== process.env.OWNER_ID) {
-					embed.setDescription("You do not have the required role to use the command.");
-					interaction.reply({ embeds: [embed], ephemeral: true });
-					return;
-				}
-			}
-
-			command.execute(interaction);
 			return;
 		} catch (error) {
 			console.error(error);
