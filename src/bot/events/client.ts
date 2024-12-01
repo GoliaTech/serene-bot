2d8b67996ec6f5918688e029e6ee8e043d2e307d
GoliaTech <damian.pwozniak@gmail.com>
2024-12-01 01:05:44 +0100

    Adding a stupid useless function lool
    This will randomly post music in predefined channels basiaclly.... that's it.
    Its music i like btw.,

--- src/bot/events/client.ts
+++ src/bot/events/client.ts
@@ -1,6 +1,7 @@
 import { Client, Events } from "discord.js";
 import { I_BotEvent } from "../../utilities/interface";
 import { logError, logGeneral } from "../../utilities/utilities";
+import { embedBuilder } from "../misc/builders";
 
 /**
  * This will set a random presence.
@@ -42,6 +43,74 @@ const ready: I_BotEvent = {
 	once: true,
 };
 
+// I dont want to redeclare these when calling the function.
+const mainChannel = "792743975273889812";
+const devChannel = "1312517585224339517";
+const musicLinkList: {
+	name: string;
+	artist: string;
+	yt: string;
+	spotify?: string;
+	year?: string;
+	album?: string;
+}[] = [
+		{
+			name: "Where's Your Head At",
+			artist: "Basement Jaxx",
+			yt: "https://music.youtube.com/watch?v=omwXLXeTR4w&si=zKox8gmIYLt_fgOR",
+			spotify: "https://open.spotify.com/track/3cJh89D0za2SW705fNBo3b?si=d8045285e24c4d4e",
+			year: "2001",
+			album: "Rooty"
+		}
+	];
+
+function musicRecommendations(client: Client) {
+	let channel = client.channels.fetch(mainChannel);
+	if (!channel) {
+		channel = client.channels.fetch(devChannel);
+	}
+	if (!channel) {
+		logError("No channel could be found for music recommendations....");
+		return;
+	}
+
+	const randomSong = musicLinkList[Math.floor(Math.random() * musicLinkList.length)];
+
+	const embed = embedBuilder("Music Recommendations")
+		.setDescription("I found this song in my music room, check it out.")
+		.setFields(
+			{ name: "Name", value: randomSong.name },
+			{ name: "Artist", value: randomSong.artist }
+			// { name: "Spotify", value: randomSong.spotify }
+		);
+	if (randomSong.year) {
+		embed.addFields({ name: "Year", value: randomSong.year });
+	}
+	if (randomSong.album) {
+		embed.addFields({ name: "Album", value: randomSong.album });
+	}
+	embed.addFields({ name: "YouTube Music", value: randomSong.yt });
+	if (randomSong.spotify) {
+		embed.addFields({ name: "Spotify", value: randomSong.spotify });
+	}
+
+	// stop being dumb TS. We are literally checking it on line 72.
+	channel.send({ embeds: [embed] });
+}
+
+const musicLinks: I_BotEvent = {
+	name: Events.ClientReady,
+	/**
+	 * This will do things when the bot is ready to launch. Run it once.
+	 * @param {Client} client - The client.
+	 */
+	execute(client: Client) {
+		setInterval(() => musicRecommendations(client), 1 * 60 * 1000);
+	},
+	once: true,
+};
+
 module.exports = [
-	ready
+	ready,
+	musicLinks
 ];
